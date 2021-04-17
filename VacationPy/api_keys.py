b17cd5bb67f49a7720d8b77e4066adcdcdce5548
AJ P <10paineaj@gmail.com>
2021-04-16 20:20:53 -0600

    Finish and clean up WeatherPy. Add VacationPy charts

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "64ecce2639273b6fface5c09d98f2cf3"
+weather_api_key = "7ba43a764f5694b99da914de38b3f751"
 
 # Google API Key
 g_key = "AIzaSyC6kbbxNCwVkYVn_-YCBXg_lm2Da97ADt0"
