24eefe7540dec770fe3fc64e30c8a06f88baf747
AJ P <10paineaj@gmail.com>
2021-04-16 22:15:24 -0600

    Fix gitignore

--- VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "7ba43a764f5694b99da914de38b3f751"
-
-# Google API Key
-g_key = "AIzaSyC6kbbxNCwVkYVn_-YCBXg_lm2Da97ADt0"
--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "64ecce2639273b6fface5c09d98f2cf3"
-
-# Google API Key
-g_key = "AIzaSyC6kbbxNCwVkYVn_-YCBXg_lm2Da97ADt0"
